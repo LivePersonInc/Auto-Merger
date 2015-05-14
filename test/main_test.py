661034b4834e40cc1821d2ad6976696cc03b58b9
tomer-ben-david <tomerbd1@gmail.com>
2015-05-14 16:31:50 +0300

    +new lines where needed

--- test/main_test.py
+++ test/main_test.py
@@ -4,8 +4,10 @@ import unittest
 from merger.conf.mergeconf import ARG_CLIENT
 from merger.utils.argutils import is_client
 
+
 class MainTest(unittest.TestCase):
 
     def test_is_client_sanity(self): self.assertFalse(is_client(['bla']))
+
     def test_is_client_when_truely_is_client(self):
         self.assertTrue(is_client(['1', ARG_CLIENT]))
